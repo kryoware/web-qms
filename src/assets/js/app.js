bcbf1d2382ba6c385a5e25aa82ad95c6e7a3651a
Baldwin John Sumatra <zer01ne2wo@gmail.com>
2019-12-25 14:05:13 +0800

    Update render logic

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,7 @@
   var AUDIO = new Audio('./assets/ding_dong.mp3'),
     MODAL_TIMEOUT = 60000,
     COUNTER_TIMEOUT = 10000,
+    REFRESH_INTERVAL = 5000,
     MAX = 2,
     CURR = 0,
     FULLSCREEN_ENABLED = false,
@@ -55,15 +56,15 @@ $(document).ready(function() {
 
   //             <div class="col text-center pd-y-20">
   //               <div class="d-flex flex-column justify-content-around">
-  //                 <p style="font-size: 2.5rem; line-height: 2.5rem;" class="text-uppercase tx-semibold mg-0">Ticket</p>
-  //                 <p style="font-size: 4rem; line-height: 4rem;" class="tx-semibold mg-0 mg-t-10">{0001}</p>
+  //                 <p style="font-size: 2.5vw; line-height: 2.5vw;" class="text-uppercase tx-semibold mg-0">Ticket</p>
+  //                 <p style="font-size: 4vw; line-height: 4vw;" class="tx-semibold mg-0 mg-t-10">{0001}</p>
   //               </div>
   //             </div>
 
   //             <div class="col text-center pd-y-20">
   //               <div class="d-flex flex-column justify-content-around">
-  //                 <p style="font-size: 2.5rem; line-height: 2.5rem;" class="text-uppercase tx-semibold mg-0">Counter</p>
-  //                 <p style="font-size: 4rem; line-height: 4rem;" class="tx-semibold mg-0 mg-t-10">${counter.id.toString().padStart(2, '0')}</p>
+  //                 <p style="font-size: 2.5vw; line-height: 2.5vw;" class="text-uppercase tx-semibold mg-0">Counter</p>
+  //                 <p style="font-size: 4vw; line-height: 4vw;" class="tx-semibold mg-0 mg-t-10">${counter.id.toString().padStart(2, '0')}</p>
   //               </div>
   //             </div>
 
@@ -170,96 +171,79 @@ $(document).ready(function() {
 // FIXME: Remove when live
 var host = 'http://dev.teaconcepts.net/CleverQMS/'
 
-function render() {
-}
-
 function loadDepartments() {
   $.ajax({
-    url: host + 'engine/api.php?act=load_departments&stat=open',
+    url: host + 'engine/api.php?act=load_departments&stat=open&details=full',
     success: function(res) {
       if (res.stat == 'ok' && res.data) {
         $('#counter_carousel .carousel-inner').html('')
         DEPARTMENTS = Object.values(res.data).forEach((dept, key) => {
+          CURR = 0
           $('#generate_ticket').parent().find('select').append(`<option value="${dept.dept_id}">${dept.dept_name}</option>`)
 
           $('#counter_carousel .carousel-inner').append(`
             <div class="carousel-item ${key == 0 ? 'active' : ''}">
               <div class="d-flex flex-column" data-dept_id="${dept.dept_id}">
-                <p class="dept-name tx-semibold mg-b-10 pd-10 tx-teal bg-white text-center text-uppercase" style="font-size: 2.75rem">${dept.dept_name}</p>
+                <p class="dept-name tx-semibold mg-b-10 pd-10 tx-teal bg-white text-center text-uppercase" style="font-size: 2.75vw">${dept.dept_name}</p>
               </div>
             </div>
           `)
+          console.log(dept.dept_name);
+          Object.values(dept.counters).forEach(counter => {
+            console.log(CURR)
+
+            if (CURR < MAX) CURR++
+            else {
+              console.log('Add new page')
+              $('#counter_carousel .carousel-inner').append(`
+                <div class="carousel-item">
+                  <div class="d-flex flex-column" data-dept_id="${dept.dept_id}">
+                    <p class="dept-name tx-semibold mg-b-10 pd-10 tx-teal bg-white text-center text-uppercase" style="font-size: 2.75vw">${dept.dept_name}</p>
+                  </div>
+                </div>
+              `)
+              CURR = 0
+            }
+
+            $('#horizontal .content').append(`
+              <div class="col">
+                <button type="button" data-counter_id="${counter.counter_id}" class="btn btn-outline-success btn-call text-uppercase">${counter.counter_id} call next</button>
+              </div>
+            `)
+
+            var containers = $('#counter_carousel').find(`[data-dept_id="${counter.dept_id}"]`)
+            $(containers[containers.length - 1]).append(`
+              <div class="bg-white mg-b-10" data-counter_id="${counter.counter_id}">
+                <div class="tx-dark">
+                  <div class="row no-gutters">
+
+                    <div class="col text-center pd-y-20">
+                      <div class="d-flex flex-column justify-content-around">
+                        <p style="font-size: 2.5vw; line-height: 2.5vw;" class="ticket-label text-uppercase tx-semibold mg-0">Ticket</p>
+                        <p style="font-size: 4vw; line-height: 4vw;" class="ticket-no tx-semibold mg-0 mg-t-10"></p>
+                      </div>
+                    </div>
+
+                    <div class="col text-center pd-y-20">
+                      <div class="d-flex flex-column justify-content-around">
+                        <p style="font-size: 2.5vw; line-height: 2.5vw;" class="counter-label text-uppercase tx-semibold mg-0">Counter</p>
+                        <p style="font-size: 4vw; line-height: 4vw;" class="counter-no tx-semibold mg-0 mg-t-10">${counter.counter_no.toString().padStart(2, '0')}</p>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            `)
+          })
         })
 
         $('#counter_carousel').carousel({
           interval: COUNTER_TIMEOUT,
           ride: 'carousel'
         })
-        loadCounters()
-      }
-    }
-  })
-}
 
-function loadCounters() {
-  $.ajax({
-    url: host + 'engine/api.php?act=load_counters&stat=open',
-    success: function(res) {
-      if (res.stat == 'ok' && res.data) {
-        Object.values(res.data).forEach(counter => {
-          if (CURR < MAX) CURR++
-          else {
-            var dept_name = $('#counter_carousel').find(`[data-dept_id="${counter.dept_id}"] .dept-name`).text()
-
-            console.log(dept_name)
-
-            $('#counter_carousel .carousel-inner').append(`
-              <div class="carousel-item">
-                <div class="d-flex flex-column" data-dept_id="${counter.dept_id}">
-                  <p class="dept-name tx-semibold mg-b-10 pd-10 tx-teal bg-white text-center text-uppercase" style="font-size: 2.75rem">${dept_name}</p>
-                </div>
-              </div>
-            `)
-            CURR = 0
-          }
-          console.log(counter)
-
-          $('#horizontal .content').append(`
-            <div class="col">
-              <button type="button" data-counter_id="${counter.counter_id}" class="btn btn-outline-success btn-call text-uppercase">${counter.counter_id} call next</button>
-            </div>
-          `)
-
-          var containers = $('#counter_carousel').find(`[data-dept_id="${counter.dept_id}"]`)
-          $(containers[containers.length - 1]).append(`
-            <div class="bg-white mg-b-10" data-counter_id="${counter.counter_id}">
-              <div class="tx-dark">
-                <div class="row no-gutters">
-
-                  <div class="col text-center pd-y-20">
-                    <div class="d-flex flex-column justify-content-around">
-                      <p style="font-size: 2.5rem; line-height: 2.5rem;" class="ticket-label text-uppercase tx-semibold mg-0">Ticket</p>
-                      <p style="font-size: 4rem; line-height: 4rem;" class="ticket-no tx-semibold mg-0 mg-t-10"></p>
-                    </div>
-                  </div>
-
-                  <div class="col text-center pd-y-20">
-                    <div class="d-flex flex-column justify-content-around">
-                      <p style="font-size: 2.5rem; line-height: 2.5rem;" class="counter-label text-uppercase tx-semibold mg-0">Counter</p>
-                      <p style="font-size: 4rem; line-height: 4rem;" class="counter-no tx-semibold mg-0 mg-t-10">${counter.counter_no.toString().padStart(2, '0')}</p>
-                    </div>
-                  </div>
-
-                </div>
-              </div>
-            </div>
-          `)
-        })
-
-        // setInterval(() => {
-        //   console.log('Refresh Tickets')
-        //   loadTickets()
-        // }, 1000)
+        setInterval(loadTickets(), REFRESH_INTERVAL)
       }
     }
   })
@@ -272,22 +256,26 @@ function loadTickets() {
     url: host + 'engine/api.php?act=load_tickets&stat=active' + dept_id,
     success: function(res) {
       if (res.stat == 'ok' && res.data) {
-        TICKETS = Object.values(res.data).forEach(deptTickets => {
+        var NEW_TICKETS = Object.values(res.data).forEach(deptTickets => {
           Object.values(deptTickets).forEach(ticket => {
             if (parseInt(ticket.counter_id) != 0) {
               var counter_card = $(`[data-dept_id="${ticket.dept_id}"]`).find(`[data-counter_id="${ticket.counter_id}"]`)
               var ticket_dom = $(counter_card).find('.ticket-no')
 
-              console.table({
-                dept_id: ticket.dept_id,
-                counter_id: ticket.counter_id,
-                ticket: ticket.ticket_label
-              })
+              // console.table({
+              //   dept_id: ticket.dept_id,
+              //   counter_id: ticket.counter_id,
+              //   ticket: ticket.ticket_label
+              // })
 
               ticket_dom.text(ticket.ticket_label)
             }
           })
         })
+
+        if (TICKETS.length != NEW_TICKETS.length) {
+            
+        }
       }
     }
   })
