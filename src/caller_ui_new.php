51996d042155fc3aae736d719b125536045f41b9
Baldwin John Sumatra <zer01ne2wo@gmail.com>
2020-01-04 21:06:53 +0800

    Update Caller UI

--- src/caller_ui_new.php
+++ src/caller_ui_new.php
@@ -21,6 +21,7 @@ $crumbs_title = "$page_title";
   <title><?php echo "$page_title"; ?></title>
   <link rel="stylesheet" href="assets/css/app.css">
   <link rel="stylesheet" href="assets/css/display.css">
+  
   <style>
     .logged-user img{
       width: 32px;
@@ -33,14 +34,31 @@ $crumbs_title = "$page_title";
     .main-content{
       height: calc(100vh - 62px) !important;
     }
-    
-    .summary-panel {
-      width: 40vw;
+
+    @media screen and (orientation: portrait) {
+      .panel-wrap {
+        flex-direction: column !important;
+      }
+
+      .summary-panel {
+        width: 100vw;
+      }
+      
+      .actions-panel {
+        width: 100vw;
+      }
     }
-    
-    .actions-panel {
-      width: 60vw;
+
+    @media screen and (orientation: landscape) {
+      .summary-panel {
+        width: 40vw;
+      }
+      
+      .actions-panel {
+        width: 60vw;
+      }
     }
+
     .summary-label {
       font-size: 1.25rem;
     }
@@ -49,7 +67,7 @@ $crumbs_title = "$page_title";
       font-size: 3rem;
     }
     .profile-pic img {
-      border: 1px solid red;
+      border: 1px solid #1CAF9A;
       border-radius: 100%;
       padding: 2px;
     }
@@ -57,36 +75,40 @@ $crumbs_title = "$page_title";
       width: 32px;
       height: 32px;
     }
+
+    .custom-oblong {
+      border-radius: 1rem;
+    }
   </style>
 </head>
 <body>
   <div class="d-flex flex-column">
     <div class="custom-header container-fluid pd-y-10 justify-content-between d-flex">
       <div class="d-flex flex-column">
-        <p class="mg-0">Counter #</p>
-        <p class="mg-0">Department</p>
+        <p class="mg-0" id="counter_no">Counter #</p>
+        <p class="mg-0" id="dept_name">Department</p>
       </div>
       
       <div class="d-flex">
         <div class="d-flex flex-column justify-content-center ht-100p">
           <div class="profile-pic">
-            <img alt="" src="http://lorempixel.com/output/cats-q-c-480-480-8.jpg" class="img-fluid">
+            <img alt="" src="http://lorempixel.com/output/cats-q-c-480-480-8.jpg" class="img-fluid" id="image">
           </div>
         </div>
         
         <div class="d-flex flex-column justify-content-center ht-100p">
-          <p class="mg-0 mg-l-10">First Name</p>
+          <p class="mg-0 mg-l-10" id="name">First Name</p>
         </div>
       </div>
     </div>
   
     <div class="main-content container-fluid pd-0 mg-0 flex-grow-1">
-      <div class="d-flex ht-100p">
+      <div class="panel-wrap d-flex ht-100p">
         <div class="summary-panel bg-teal">
           <div class="d-flex flex-column justify-content-between pd-10 ht-100p">
 
             <!-- SUMMARY -->
-            <div class="custom-rounded d-flex flex-column pd-10 bg-white flex-grow-1 mg-b-10">
+            <div class="custom-oblong d-flex flex-column pd-10 bg-white flex-grow-1 mg-b-10">
 
               <div class="d-flex flex-column flex-grow-1 ">
                 <p class="summary-label mg-0 text-center text-uppercase">Serving</p>
@@ -95,7 +117,7 @@ $crumbs_title = "$page_title";
 
               <div class="d-flex flex-column justify-content-between">
                 <p class="summary-label mg-0 text-center text-uppercase">Time Elapsed</p>
-                <p class="wd-75p mx-auto custom-rounded pd-10 bg-teal tx-white mg-0 text-center" id="time">00:00</p>
+                <p class="wd-75p mx-auto custom-oblong pd-10 bg-teal tx-white mg-0 text-center" id="time">00:00</p>
               </div>
 
             </div>
@@ -103,8 +125,8 @@ $crumbs_title = "$page_title";
 
 
             <!-- STATISTICS-->
-            <div class="custom-rounded d-flex flex-column pd-10 bg-white" style="font-size: 2.5vmax">
-              <p class="text-center text-uppercase tx-semibold tx-teal mg-0">Statistics</p>
+            <div class="custom-oblong d-flex flex-column pd-x-25 pd-y-10 bg-white" style="font-size: 2.5vmax">
+              <p class="text-center text-uppercase tx-semibold tx-teal mg-0 mg-b-10">Statistics</p>
 
               <div class="d-flex justify-content-between">
 
@@ -131,18 +153,18 @@ $crumbs_title = "$page_title";
           </div>
         </div>
 
-        <div class="actions-panel bg-gray-400">
+        <div class="actions-panel bg-gray-400 flex-grow-1">
           <div class="d-flex flex-column pd-10 ht-100p">
-            <button class="tx-semibold tx-uppercase custom-rounded btn btn-teal btn-block flex-grow-1 mg-b-5">Next</button>
+            <button class="tx-semibold tx-uppercase custom-oblong btn btn-teal btn-block flex-grow-1 mg-b-5" id="caller_next">Next</button>
 
             <div class="d-flex justify-content-between flex-grow-1 mg-y-5">
-              <button class="tx-semibold tx-uppercase custom-rounded btn btn-warning wd-50p mg-r-5">Recall</button>
-              <button class="tx-semibold tx-uppercase custom-rounded btn btn-success wd-50p mg-l-5">Done</button>
+              <button class="tx-semibold tx-uppercase custom-oblong btn btn-warning wd-50p mg-r-5" id="caller_recall">Recall</button>
+              <button class="tx-semibold tx-uppercase custom-oblong btn btn-success wd-50p mg-l-5" id="caller_done">Done</button>
             </div>
 
             <div class="d-flex justify-content-between flex-grow-1 mg-t-5">
-              <button class="tx-semibold tx-uppercase custom-rounded btn btn-secondary wd-50p mg-r-5">No Show</button>
-              <button class="tx-semibold tx-uppercase custom-rounded btn btn-danger wd-50p mg-l-5">Cancel</button>
+              <button class="tx-semibold tx-uppercase custom-oblong btn btn-secondary wd-50p mg-r-5" id="caller_no_show">No Show</button>
+              <button class="tx-semibold tx-uppercase custom-oblong btn btn-danger wd-50p mg-l-5" id="caller_cancel">Cancel</button>
             </div>
 
           </div>
